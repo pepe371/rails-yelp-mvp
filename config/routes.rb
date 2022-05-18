0d38955170d8ae8961e5caa5f1e78faa4219a795
jose garcia <joey.garcia3711@gmail.com>
2022-05-18 17:02:08 -0500

    hard

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # /restaurants/...
+  resources :restaurants, only: %i[index show new create] do
+    # .../reviews/new
+    resources :reviews, only: [ :new, :create ]
+  end
+
+  # resources :restaurants do
+  #   resources :reviews, only: [ :new, :create ]
+  # end
+  # resources :reviews, only: [ :destroy ]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating restaurants"
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  5.times do
+    Review.create!(restaurant: restaurant, content: Faker::Restaurant.review, rating: rand(0..5))
+  end
+end
+puts "Done"
